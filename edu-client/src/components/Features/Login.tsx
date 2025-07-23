fb61a1182de74af639ca034223375da343af1962
Madesh <madeshdex10@gmail.com>
2025-07-23 21:20:45 +0530

    enhanced the login feature

--- edu-client/src/components/Features/Login.tsx
+++ edu-client/src/components/Features/Login.tsx
@@ -1,110 +1,143 @@
-import '../../styles/Components/_login.scss';
-import { NavLink } from 'react-router-dom';
-import {  useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import "../../styles/Components/_login.scss";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
+function Login({
+  setIsAuthenticated,
+  setRole,
+}: {
+  setIsAuthenticated: (value: boolean) => void;
+  setRole: (value: string) => void;
+}) {
+  const navigate = useNavigate();
 
-function Login({setIsAuthenticated}: {setIsAuthenticated: (value:boolean)=>void}) {
+  const [userLogin, setuserLogin] = useState({
+    email: "",
+    password: "",
+  });
 
-    const navigate = useNavigate()
+  useEffect(() => {
+    const verifedToken = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        return;
+      }
 
-    const[userLogin,setuserLogin] = useState({
-        email:'',
-        password:''
-    })
+      try {
+        await axios.get("http://localhost:8080/auth/verify", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
 
-    useEffect(()=>{
-        const verifedToken = async()=>{
-            const token = localStorage.getItem('token');
-            if(!token){
-                return 
-            }
+        const role = localStorage.getItem("role");
 
-            try{
-                await axios.get('http://localhost:8080/auth/verify',{
-                    headers:{Authorization:`Bearer ${token}`}
-                })
-                navigate('/authenticated-home')
-            }catch(err){
-                localStorage.removeItem('token')
-            }
-        };
-        verifedToken()
-    },[navigate])
-
-    const HandleUserLogin = async(e:React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        try{
-            const response = await  axios.post('http://localhost:8080/auth/login',userLogin)
-            console.log(response.data)
-            localStorage.setItem("token", response.data.token);
-            localStorage.setItem("role",response.data.role)
-            setuserLogin({
-                email:'',
-                password:''
-            })
-            setIsAuthenticated(true)
-
-            const role = response.data.role;
-
-            if(role === 'instructor'){
-                navigate('/instructor-dashboard')
-            }
-            else{
-                navigate('/authenticated-home')
-            }
+        if (role === "instructor") {
+          navigate("/instructor-dashboard");
+        } else {
+          navigate("/authenticated-home");
         }
-        catch(err){
-            if(axios.isAxiosError(err)){
-                // Give a server error
-                console.error(err.response?.data);
-                navigate('/login');
-            }
-        }
+      } catch (err) {
+        localStorage.removeItem("token");
+      }
+    };
+    verifedToken();
+  }, []);
+
+  const HandleUserLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/auth/login",
+        userLogin
+      );
+      console.log(response.data);
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("role", response.data.role);
+
+      setuserLogin({
+        email: "",
+        password: "",
+      });
+      setIsAuthenticated(true);
+
+      const role = response.data.role;
+      setRole(role);
+
+      if (role === "instructor") {
+        localStorage.setItem("instructorId", response.data.userId);
+      }
+      navigate("/authenticated-home");
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        // Give a server error
+        console.error(err.response?.data);
+        navigate("/login");
+      }
     }
+  };
 
   return (
     <section>
-        <div className="Login-container">
-            <div className='Login-Card'>
-                <div className='Login-content'>
-                    <h1>Login</h1>
-                    <div className='Login-form'>
-                        <form action="" onSubmit={HandleUserLogin}>
-                            <div className='Email'>
-                                <input type="email" name='email' placeholder='Email' value={userLogin.email} onChange={(e)=> setuserLogin({...userLogin, email: e.target.value})} />
-                            </div>
-                            <div className='Password'>
-                                <input type="password" name='password' placeholder='Password' value={userLogin.password}  onChange={(e)=>setuserLogin({...userLogin,password: e.target.value})}/>
-                            </div>
-                            <button className='Login-btn'type='submit' >Login</button>
-                            <div className='Remember-box'>
-                                <div className='Remember-input'>
-                                    <input type="checkbox" />
-                                    <p>Remember Me</p>
-                                </div>
-                                <div className='Forget-password'>
-                                    <NavLink to='/'>Forgot your password</NavLink>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <hr className='Login-line' />
-                    <div className='Not-Member'>
-                       <p>Not a Member Yet?</p>
-                       <p className='sign-up-btn'><NavLink to='/SignUp'>Sign up</NavLink></p> 
-                    </div>
+      <div className="Login-container">
+        <div className="Login-Card">
+          <div className="Login-content">
+            <h1>Login</h1>
+            <div className="Login-form">
+              <form action="" onSubmit={HandleUserLogin}>
+                <div className="Email">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={userLogin.email}
+                    onChange={(e) =>
+                      setuserLogin({ ...userLogin, email: e.target.value })
+                    }
+                  />
                 </div>
+                <div className="Password">
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={userLogin.password}
+                    onChange={(e) =>
+                      setuserLogin({ ...userLogin, password: e.target.value })
+                    }
+                  />
+                </div>
+                <button className="Login-btn" type="submit">
+                  Login
+                </button>
+                <div className="Remember-box">
+                  <div className="Remember-input">
+                    <input type="checkbox" />
+                    <p>Remember Me</p>
+                  </div>
+                  <div className="Forget-password">
+                    <NavLink to="/">Forgot your password</NavLink>
+                  </div>
+                </div>
+              </form>
             </div>
+            <hr className="Login-line" />
+            <div className="Not-Member">
+              <p>Not a Member Yet?</p>
+              <p className="sign-up-btn">
+                <NavLink to="/SignUp">Sign up</NavLink>
+              </p>
+            </div>
+          </div>
         </div>
-        <div className='CopyRight'>
-            <p className='privacy'>Privacy PolicyTerms of Use</p>
-            <p>Copyright © 2025 by Udemy, Inc. - All rights reserved.</p>
-        </div>
+      </div>
+      <div className="CopyRight">
+        <p className="privacy">Privacy PolicyTerms of Use</p>
+        <p>Copyright © 2025 by Udemy, Inc. - All rights reserved.</p>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- edu-client/src/components/isAuthenticatedHome/Navabar/Navbar.tsx
+++ edu-client/src/components/isAuthenticatedHome/Navabar/Navbar.tsx
@@ -5,9 +5,17 @@ import { IoCartOutline } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
 import { CiHeart } from "react-icons/ci";
 import { CiBellOn } from "react-icons/ci";
+import { useEffect, useState } from 'react';
 
 function AuthNavbar({setIsAuthenticated}: {setIsAuthenticated:(value:boolean)=>void}) {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [role,setrole] = useState<string | null>(null)
+
+    useEffect(()=>{
+        const storedRole = localStorage.getItem("role")
+        setrole(storedRole);
+    },[])
+
     const handleLogout = ()=>{
         localStorage.removeItem("token")
         setIsAuthenticated(false)
@@ -27,7 +35,8 @@ function AuthNavbar({setIsAuthenticated}: {setIsAuthenticated:(value:boolean)=>v
                 <div className='nav-list-item'>
                     <ul>
                         <li><NavLink to='/plan-pricing'>Udemy Business</NavLink></li>
-                        <li><NavLink to='/teacher'>Instructor</NavLink></li>
+                        {role === 'instructor' ? (<li><NavLink to='/instructor-dashboard'>Instructor</NavLink></li>):<li><NavLink to='/authenticated-home'>New Learners</NavLink></li>}
+                        
                         <li><NavLink to='/Tech on Udemy'>My learning</NavLink></li>
                         <li><NavLink to=''><span><CiHeart/></span></NavLink></li>
                         <li className='Cart'><NavLink to='/cart'><span><IoCartOutline/></span></NavLink></li>
